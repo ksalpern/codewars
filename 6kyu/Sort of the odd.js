510591e51e57bc4ed4baaa3bdb09a6e1846e82b3
Ksenia PI <101118611+ksalpern@users.noreply.github.com>
2022-07-19 11:15:42 +0300

    create Sort of the odd.js

---
+++ 6kyu/Sort of the odd.js
@@ -0,0 +1,18 @@
+// Task
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+//Solution
+
+function sortArray(array) {
+  // Return a sorted array.
+  const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+  return array.map((x) => x % 2 ? odd.shift() : x);
+}
+//filter: create a filtered array that has all elements with remainder of the division
+//sort: the compare function can subtract b from a. The following function will sort the array in ascending order (if it doesn't contain Infinity and NaN)
+// map: returns new array, ternary operator adds remainder if true or returns the number if false
